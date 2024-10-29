1b2c61f4c247012afe42666fcf8aa0909be61d41
HOUDA KHADIRI <161170011+hudakhadiri@users.noreply.github.com>
2024-10-29 09:36:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -25,20 +25,31 @@ with st.sidebar:
     st.header("Settings")
     website_url = st.text_input("Website URL")
 
-user = st.chat_input("Enter votre text ...")
-
-if user is not None and user !="" :
-    reply = reponse(user)
-    st.session_state.chat_history.append(HumanMessage(content=user))
-    st.session_state.chat_history.append(AIMessage(content=reply))
 
 
-# aconversation
-for msg in st.session_state.chat_history:
-    if isinstance(msg,AIMessage):
-        with st.chat_message("AI"):
-            st.write(msg.content)
+if website_url is None or website_url == "":
+    st.info("Please enter a website URL")
 
-    elif isinstance(msg,HumanMessage):
-        with st.chat_message("Human"):
-            st.write(msg.content)
+else:
+    # session state
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = [
+            AIMessage(content="Hello, I am a bot. How can I help you?"),
+        ]
+
+    # user input
+    user_query = st.chat_input("Type your message here...")
+    if user_query is not None and user_query != "":
+        response = reponse(user_query)
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+        st.session_state.chat_history.append(AIMessage(content=response))
+        
+
+    # conversation
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
+            with st.chat_message("AI"):
+                st.write(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message("Human"):
+                st.write(message.content)
